178a0b8221c1555cf4b63d70c2d16ed78a1023a8
Your NameTomasDVB <you@example.com>
2022-07-30 06:37:21 -0300

    fix homework1's code and start homework2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -239,11 +239,9 @@ function esVocal(letra){
   //Escribe tu código aquí
   if (letra.length > 1 ) {
     return "Dato incorrecto";
-  }
-  else if (letra === 'a' | letra === 'e' | letra === 'i' | letra === 'o' | letra === 'u') {
+  } else if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') {
     return "Es vocal";
-  }
-  else {
+  } else {
     return "Dato incorrecto";
   }
 }
